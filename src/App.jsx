59ace75de22c5fb0b2505fc99698b130699bc9d2
ankit <ankitsam0602@gmail.com>
2024-11-30 23:10:01 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './sections/header.jsx';
+import Hero from './sections/hero.jsx';
+
+const App = () => {
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+    </main>
+  );
+};
+export default App;
