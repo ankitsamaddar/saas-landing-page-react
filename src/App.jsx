2686224075783707a393f14e0b57a6d764f3ffa3
ankit <ankitsam0602@gmail.com>
2024-12-01 02:59:56 +0530

    💡commit: add the features section✨

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
 import Header from './sections/header.jsx';
 import Hero from './sections/hero.jsx';
+import Features from './sections/features.jsx';
 
 const App = () => {
   return (
     <main className="overflow-hidden">
       <Header />
       <Hero />
+      <Features />
     </main>
   );
 };
